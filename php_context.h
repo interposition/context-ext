1b9d53cf44cf98ca45fbe18c95dfe9bc580a42c5
pc <as@we.er>
2021-04-27 21:52:00 +0700

    Add major version check.

--- php_context.h
+++ php_context.h
@@ -3,6 +3,10 @@
 #ifndef PHP_CONTEXT_H
 # define PHP_CONTEXT_H
 
+#if PHP_MAJOR_VERSION < 8
+#error Minimum major version: 8.
+#endif
+
 extern zend_module_entry context_module_entry;
 # define phpext_context_ptr &context_module_entry
 
